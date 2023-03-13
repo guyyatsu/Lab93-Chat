eb44baf4dd997016092f18bb071a4a0f917289a0
Hunter Dale <guyyatsu@gmail.com>
2023-03-12 21:33:18 -0400

    Working on a build system

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,12 @@ ProjectBin=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Activate te development environment.
 source $ProjectBin/activate
 
+cd $ProjectBin/..
+
+python3 $ProjectBin/IncrementVersion.py $ProjectBin/..pyproject.toml
+python3 -m build
+
+
 
 
 # Obsolete; fuck jupyter. See Issue #9.
